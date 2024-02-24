1eb9af2d27637486ada835eb8e69500d85db9a1a
Adrian <toledosandro59@gmail.com>
2024-02-23 22:19:39 -0500

    sliders con json

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
-const swiper = new Swiper('.swiper-products', {
-    // Optional parameters
-    loop: true,
-    allowTouchMove: true,
-    cssMode: true,
+document.addEventListener('DOMContentLoaded', function () {
+    // Fetch your data
+    fetch('recomends.json')
+        .then(response => response.json())
+        .then(data => {
+            const container = document.querySelector('.swiper-container-products');
+            const swiperWrapper = document.querySelector('.swiper-slide-products');
 
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
+            swiperWrapper.style.display = 'none';
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
 
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
+
+            data.forEach(reco => {
+
+                //clonar template
+                const card = swiperWrapper.cloneNode(true);
+
+                //modificar datos
+                card.querySelector('.tittle-products').textContent = reco.titulo;
+                card.querySelector('.subtittle-products').textContent = `Cera: ${reco.subtitulo}`;
+                card.querySelector('.overview').textContent = `Esencia: ${reco.descripcion}`;
+                card.querySelector('.img img').src = reco.imagen;
+
+                //quitamos el display none
+                card.style.display = 'flex';
+
+                //añadir al DOM
+                container.appendChild(card);
+
+            });
+
+        });
 });
 
